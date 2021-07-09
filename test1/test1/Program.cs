c5c6e1e0f2f4ea2b0c5e321f5811821120daf640
Bryushkov Ilya <bryushkov.i@yandex.ru>
2021-07-09 14:17:53 +0300

    Получение чисел из файла

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace test1
     {
         static void Main(string[] args)
         {
+            string Path = Console.ReadLine();
 
+            StreamReader sr = new StreamReader(Path);
+
+            List<int> list = new List<int>();
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                list.Add(Convert.ToInt32(line));
+            }
+
+            foreach (var e in list) Console.WriteLine(e);
+
+            Console.ReadKey();
+
+            /*
+             * упорядочить числа
+             * 90 перценталь
+             * медиана
+             * максимальное значение
+             * минимальное значение
+             * среднее значение
+            */
         }
     }
 }
