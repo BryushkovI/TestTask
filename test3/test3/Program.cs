52b649a9054dd6ee4f4de3da9ab525e6e16bdb0a
Bryushkov Ilya <bryushkov.i@yandex.ru>
2021-07-10 10:31:45 +0300

    Готова 3 задача

--- test3/test3/Program.cs
+++ test3/test3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace test3
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            StreamReader sr;
+            double MaxVisitres = 0;
+            int NumMaxInterval = 0;
+            for(int i = 0; i < 4; i++)
+            {
+                sr = new StreamReader($"Cash{i + 1}.txt");
+                string line;
+                List<double> Intervals = new List<double>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.Trim('\\','n');
+                    Intervals.Add(double.Parse(line));
+                    if (MaxVisitres < double.Parse(line))
+                    {
+                        MaxVisitres = double.Parse(line);
+                        NumMaxInterval = Intervals.Count();
+                    }
+                }
+                sr.Close();
+            }
+            Console.WriteLine(NumMaxInterval + "\n");
+            Console.ReadLine();
         }
     }
 }
